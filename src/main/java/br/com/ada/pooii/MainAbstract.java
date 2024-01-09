d1a0c288797f519154ae17812b6aa2471b0e261b
suersil <suelenregsilva@gmail.com>
2024-01-09 17:21:03 -0300

    Aula Interface

---
+++ src/main/java/br/com/ada/pooii/MainAbstract.java
@@ -0,0 +1,42 @@
+package br.com.ada.pooii;
+public class Main {
+    public static void main(String[] args) {
+
+        ContaCorrente cc = new ContaCorrente();
+
+        cc.depositar(10.00);
+
+        System.out.println(cc.getSaldo());
+    }
+}
+abstract class Conta {
+
+    //Atributes (encapsulados/wrapper):
+
+    //private String titular; - Mudamos para PROTECTED
+    // para podermos usar na classe ContaCorrente que herda(extends) da classe Conta
+    protected String titular;
+
+    //private double saldo; -  Mudamos para PROTECTED
+    protected double saldo;
+
+    //Methods
+    abstract void depositar (double valor);
+
+    abstract void sacar (double valor);
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
+
+class ContaCorrente extends Conta{
+    @Override
+    void depositar(double valor) {
+        this.saldo += valor * 0.95;  //cobrando 5% de juros  //This. ou Super. ambos servem
+    }
+    @Override
+    void sacar(double valor) {
+        this.saldo -= valor;
+    }
+}
